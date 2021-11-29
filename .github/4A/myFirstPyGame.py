2b08a8002b332c007f954b3a2c5d6d1e050c6504
Julian g <90346616+XDTRASHXD@users.noreply.github.com>
2021-11-29 14:17:23 -0500

    v0.2 [4A]

--- .github/4A/myFirstPyGame.py
+++ .github/4A/myFirstPyGame.py
@@ -1,5 +1,10 @@
-#My First PyGame, Julian Guevara, 11/29/21 2:10pm, v0.1
+#My First PyGame, Julian Guevara, 11/29/21 2:10pm, v0.2
 
 import pygame, sys
 from pygame.locals import *
 
+#Start Pygame
+pygame.init()
+
+#setup our window. 1
+windowSurface = pygame.display.set_mode((500,400),0,32)
