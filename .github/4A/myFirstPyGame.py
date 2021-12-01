5ba44f1390fed29c380da1f477f74aa28f8451ea
Julian g <90346616+XDTRASHXD@users.noreply.github.com>
2021-12-01 13:56:19 -0500

    v0.6 [4A]
    added line color and points

--- .github/4A/myFirstPyGame.py
+++ .github/4A/myFirstPyGame.py
@@ -1,6 +1,7 @@
-#My First PyGame, Julian Guevara, 11/29/21 2:50pm, v0.5
+#My First PyGame, Julian Guevara, 12/1/21 1:55pm, v0.6
 
 import pygame, sys
+from pygame import draw
 from pygame.locals import *
 
 #Start Pygame
@@ -16,6 +17,7 @@ WHITE = (255,255,255)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+NOTRED = (2,140,155)
 
 # Setup font.
 basicFont = pygame.font.SysFont(None,48)
@@ -27,7 +29,12 @@ textRect.centerx= windowSurface.get_rect().centerx
 textRect.centery= windowSurface.get_rect().centery
 
 # Fill background color.
-windowSurface.fill(WHITE)
+windowSurface.fill(white)
 
 # Draw a polygon onto the screen.
-pygame.draw.polygon(windowSurface, BLACK, ((146,0),(291,106),(236,277),(56,277),(0,106)))
+pygame.draw.polygon(windowSurface, BLACK, ((146,0), (291,106), (236,277), (56,277), (0,106)))
+
+# Draw lines on the screen
+pygame.draw.line(windowSurface, GREEN, (60,60), (120,60), 4)
+pygame.draw.line(windowSurface, BLUE, (256,300), (300,400), 3)
+pygame.draw.line(windowSurface, NOTRED, (100,200), (140, 235), 2)
